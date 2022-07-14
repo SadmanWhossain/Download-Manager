49bad1030326d2dcd12716f53f4786e154b60483
SadmanWhossain <72847790+SadmanWhossain@users.noreply.github.com>
2022-07-14 16:52:40 +0600

    init

---
+++ main.py
@@ -0,0 +1,102 @@
+from os import scandir, rename
+from os.path import splitext, exists
+from shutil import move
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+source_dir = "C:/Users/Sadman/Downloads"
+dest_dir_doc = "C:/Users/Sadman/Downloads/Document"
+dest_dir_sfx = "C:/Users/Sadman/Downloads/sfx"
+dest_dir_music = "C:/Users/Sadman/Downloads/Music"
+dest_dir_video = "C:/Users/Sadman/Downloads/Video"
+dest_dir_image = "C:/Users/Sadman/Downloads/Image"
+
+file_lists = scandir(source_dir)
+for filename in file_lists:
+    print(filename.name)
+image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw",
+                    ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
+
+video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
+                    ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
+
+audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
+
+document_extensions = [".doc", ".docx", ".odt",
+                       ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+
+def make_unique(path):
+    filename, extension = splitext(path)
+    counter = 1
+    # * IF FILE EXISTS, ADDS NUMBER TO THE END OF THE FILENAME
+    while exists(path):
+        path = f"{filename} ({counter}){extension}"
+        counter += 1
+
+    return path
+
+
+def move_file(dest, entry, name):
+    if exists(f"{dest}/{name}"):
+        unique_name = make_unique(name)
+        rename(entry, unique_name)
+    move(entry, dest)
+    print(f"moved, {name}")
+
+
+class MoverHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        with scandir(source_dir) as entries:
+            for entry in entries:
+                name = entry.name
+                self.check_audio_files(entry, name)
+                self.check_video_files(entry, name)
+                self.check_image_files(entry, name)
+                self.check_document_files(entry, name)
+
+    def check_audio_files(self, entry, name):  # * Checks all Audio Files
+        for audio_extension in audio_extensions:
+            if name.endswith(audio_extension) or name.endswith(audio_extension.upper()):
+                if entry.stat().st_size < 10_000_000 or "SFX" in name:  # ? 10Megabytes
+                    dest = dest_dir_sfx
+                else:
+                    dest = dest_dir_music
+                move_file(dest, entry, name)
+                logging.info(f"Moved audio file: {name}")
+
+    def check_video_files(self, entry, name):  # * Checks all Video Files
+        for video_extension in video_extensions:
+            if name.endswith(video_extension) or name.endswith(video_extension.upper()):
+                move_file(dest_dir_video, entry, name)
+                logging.info(f"Moved video file: {name}")
+
+    def check_image_files(self, entry, name):  # * Checks all Image Files
+        for image_extension in image_extensions:
+            if name.endswith(image_extension) or name.endswith(image_extension.upper()):
+                move_file(dest_dir_image, entry, name)
+                logging.info(f"Moved image file: {name}")
+
+    def check_document_files(self, entry, name):
+        for document_extension in document_extensions:
+            if name.endswith(document_extension) or name.endswith(document_extension.upper()):
+                dest = dest_dir_doc
+                move_file(dest, entry, name)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = source_dir
+    event_handler = MoverHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
