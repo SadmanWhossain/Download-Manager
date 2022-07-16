16d1ea33617732a450eb1ed17d4083b532159026
SadmanWhossain <72847790+SadmanWhossain@users.noreply.github.com>
2022-07-16 14:13:10 +0600

    Minor changes and added readme

---
+++ README.md
@@ -0,0 +1,1 @@
+#In Device Download Manager
--- main.py
+++ main.py
@@ -12,6 +12,8 @@ dest_dir_sfx = "C:/Users/Sadman/Downloads/sfx"
 dest_dir_music = "C:/Users/Sadman/Downloads/Music"
 dest_dir_video = "C:/Users/Sadman/Downloads/Video"
 dest_dir_image = "C:/Users/Sadman/Downloads/Image"
+dest_dir_compressed = "C:/Users/Sadman/Downloads/Compressed"
+dest_dir_programs = "C:/Users/Sadman/Downloads/Programs"
 
 file_lists = scandir(source_dir)
 for filename in file_lists:
@@ -20,9 +22,13 @@ image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".
                     ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
 
 video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
-                    ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
+                    ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd", ".mkv"]
 
 audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
+
+program_extensions = [".exe", ".xcf", ".msi"]
+
+compressed_extensions = [".zip", ".rar"]
 
 document_extensions = [".doc", ".docx", ".odt",
                        ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
@@ -55,6 +61,8 @@ class MoverHandler(FileSystemEventHandler):
                 self.check_video_files(entry, name)
                 self.check_image_files(entry, name)
                 self.check_document_files(entry, name)
+                self.check_compressed_files(entry, name)
+                self.check_program_files(entry, name)
 
     def check_audio_files(self, entry, name):  # * Checks all Audio Files
         for audio_extension in audio_extensions:
@@ -71,6 +79,18 @@ class MoverHandler(FileSystemEventHandler):
             if name.endswith(video_extension) or name.endswith(video_extension.upper()):
                 move_file(dest_dir_video, entry, name)
                 logging.info(f"Moved video file: {name}")
+
+    def check_compressed_files(self, entry, name):
+        for compressed_extension in compressed_extensions:
+            if name.endswith(compressed_extension) or name.endswith(compressed_extension.upper()):
+                move_file(dest_dir_compressed, entry, name)
+                logging.info(f"Moved Compressed file: {name}")
+
+    def check_program_files(self, entry, name):
+        for program_extension in program_extensions:
+            if name.endswith(program_extension) or name.endswith(program_extension.upper()):
+                move_file(dest_dir_programs, entry, name)
+                logging.info(f"Moved Program file: {name}")
 
     def check_image_files(self, entry, name):  # * Checks all Image Files
         for image_extension in image_extensions:
